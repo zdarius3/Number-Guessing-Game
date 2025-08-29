7a9c78196368f3e046892f9a9526f3a2fe19bfc3
Damian <damian.romalvarez@gmail.com>
2025-08-28 22:06:00 -0400

    Add .gitignore file

--- obj/Debug/net9.0/NumberGuessingGame.AssemblyInfo.cs
+++ obj/Debug/net9.0/NumberGuessingGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff63d9b8f54c542a6179d7205a88e32aa0f5395")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
